2df3e6406faaaacbff389c3cb545c5133d7f3b3b
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2023-03-30 20:51:08 -0300

    agregada funcionalidad caballo

--- ejercicio_individual/src/caballo.rs
+++ ejercicio_individual/src/caballo.rs
@@ -14,11 +14,14 @@ impl Caballo{
 
 impl Pieza for Caballo {
     fn puede_capturar(&self, pieza: &dyn Pieza) -> bool {
-        if pieza.color() == self.color(){
-            return true;
-        }
-        return false;
+        if pieza.color() == self.color(){ return false; }
 
+        let x_diff: u8 = self.posicion().0.abs_diff(pieza.posicion().0);
+        let y_diff: u8 = self.posicion().1.abs_diff(pieza.posicion().1);
+
+        match(x_diff, y_diff){
+            (x, y) => (x == 1 && y == 2) || (x == 2 && y == 1)
+        }
     }
 
     fn color(&self) -> &Color {
