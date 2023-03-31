3a08246bb747af69a84b1b1ed1e1d38f17ab2276
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2023-03-31 17:04:10 -0300

    prueba

---
+++ ejercicio_individual/target/debug/deps/ejercicio_individual-4fa55c98cd4fc677.d
@@ -0,0 +1,8 @@
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-4fa55c98cd4fc677.rmeta: src/main.rs Cargo.toml
+
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-4fa55c98cd4fc677.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
