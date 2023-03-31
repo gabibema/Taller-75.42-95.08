94974b9a925e0b3fb8f556f3679c190cc9737cdb
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2023-03-31 17:02:07 -0300

    prueba

---
+++ ejercicio_individual/target/debug/deps/ejercicio_individual-0616861677c9d2f3.d
@@ -0,0 +1,8 @@
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-0616861677c9d2f3: src/main.rs src/archivos.rs src/pieza.rs src/peon.rs
+
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-0616861677c9d2f3.d: src/main.rs src/archivos.rs src/pieza.rs src/peon.rs
+
+src/main.rs:
+src/archivos.rs:
+src/pieza.rs:
+src/peon.rs:
---
+++ ejercicio_individual/target/debug/deps/ejercicio_individual-e4ef52f156509f84.d
@@ -0,0 +1,5 @@
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-e4ef52f156509f84: src/main.rs
+
+/mnt/d/Gaby/Documents/AFiuba/Taller/ejercicio_individual/target/debug/deps/ejercicio_individual-e4ef52f156509f84.d: src/main.rs
+
+src/main.rs:
